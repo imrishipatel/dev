6cef6bd7d0fce351d849378cf22c310eda3d78ee
iterics-rishi <developer.iterics@gmail.com>
2022-03-22 20:38:44 -0400

    CDI

--- code/spring boot masterClass/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
+++ code/spring boot masterClass/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsScopeApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.in28minutes.spring.basics.springin5steps.basic.BinarySearchImpl;
+import com.in28minutes.spring.basics.springin5steps.cdi.SomeCDIBusiness;
+import com.in28minutes.spring.basics.springin5steps.cdi.SomeCdiDAO;
 import com.in28minutes.spring.basics.springin5steps.scope.PersonDAO;
 
 @SpringBootApplication
@@ -15,19 +17,14 @@ public class SpringIn5StepsScopeApplication {
 	private static Logger LOG = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
 	
 	public static void main(String[] args) {
-		
-//		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm()); 
-		
-		
+				
 		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsScopeApplication.class, args);
-		PersonDAO personDao = applicationContext.getBean(PersonDAO.class); 
-		PersonDAO personDao2 = applicationContext.getBean(PersonDAO.class); 
 		
-		LOG.info("{}", personDao);
-		LOG.info("{}", personDao.getJdbcConnection());
-		LOG.info("{}", personDao.getJdbcConnection());
-		LOG.info("{}", personDao2);
-		LOG.info("{}", personDao2.getJdbcConnection());
+		SomeCDIBusiness business =applicationContext.getBean(SomeCDIBusiness.class); 
+		SomeCdiDAO cdiDao = applicationContext.getBean(SomeCdiDAO.class); 
+		PersonDAO personDao = applicationContext.getBean(PersonDAO.class); 
+		
+		LOG.info("{} dao --{}", business, business.getSomeCdiDAO());
 	}
 
 }
