3d517d948a34fc5d8a1e33fce96268ce9b641a6d
iterics-rishi <developer.iterics@gmail.com>
2022-03-23 23:51:20 -0400

    added application log

--- code/spring boot masterClass/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ code/spring boot masterClass/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -17,17 +17,19 @@ public class SpringIn5StepsBasicApplication {
 		
 //		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm()); 
 		
+		try(AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class)) {
+			BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class); 
+			BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class); 
+			
+			
+			System.out.println(binarySearch1);
+			System.out.println(binarySearch);
+			
+			int result = binarySearch.binarySearch(new int[] {12,4,6}, 3);
+			System.out.println(result);
+			applicationContext.close();
+		}
 		
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringIn5StepsBasicApplication.class);
-		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class); 
-		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class); 
-		
-		
-		System.out.println(binarySearch1);
-		System.out.println(binarySearch);
-		
-		int result = binarySearch.binarySearch(new int[] {12,4,6}, 3);
-		System.out.println(result);
 	}
 
 }
